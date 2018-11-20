d32ea02808efae2ef6e3ed591c7f3f80bbbe1f84
jeffhung44 <45085981+jeffhung44@users.noreply.github.com>
2018-11-20 10:15:04 +0800

    Update Perl_delete.pl
    Version 2

--- Perl_delete.pl
+++ Perl_delete.pl
@@ -1,12 +1,12 @@
 #ARGV[0] Input file DIR;
 #ARGV[1] Output file DIR;
-#execute perl [program] [Input DIR] [Output DIR]
+#execute perl [program] [Input DIR] [Output DIR] ["Delete_content"] 
+#for example perl delete_content.pl ./ ./rst "task satam\;.*endtask"
 
 use warnings;
 use strict;
 
-
-
+my $verbose =1;
 my $dir = $ARGV[0];     #Input files DIR
 my $out_dir = $ARGV[1]; #Output files DIR
 my $dir_handle;
@@ -19,11 +19,7 @@ my $fail_cnt = 0;
 my $success_cnt = 0;
 my @fail_files;
 my @success_files;
-#text need to delete in the file
-my $delete_something = "this is add line
-it can add multiple lines
-third line here";    
-#text need to delete in the file
+my $delete_something = qr/$ARGV[2]/s;
 
 
 mkdir($out_dir) unless(-d $out_dir); #create outout files dir;
@@ -36,45 +32,47 @@ foreach $file (readdir $dir_handle){
     
     local $/= undef;
     
-    next if($file =~ /^\./);               #process all folder except . 
-    next if($file =~ /^\.\.$/);             #process all folder except ..
+    #next if($file =~ /^\./);               #process all folder except . 
+    #next if($file =~ /^\.\.$/);            #process all folder except ..
+    next if($file =~ /^[.]/);               #skip with .
     open $file_handle,'<',$file or die " can not open $file : $!\n";
     $file_out = "$file.new";                #Output file name with suffix .new
     open $file_out_handle, '>>', "$out_dir/$file_out" or die " can not open $file_out :$!\n";
-    print $file_out_handle "this is the first line!\n";    #add some text to first line;
+    #print $file_out_handle "this is the first line!\n";    #add some text to first line;
     
     my $fullstring = do{local $/;<$file_handle>};       ###### Read Full File into String
     
     open $file_handle,'<',$file or die "can not open $file :$!\n";
-    if ($fullstring =~ m/$delete_something/g){
-            print "Found same content in: ".$file."\n";
+    if ($fullstring =~ m/$delete_something/){
+            # print "Found same content in: ".$file."\n";
             $success_cnt++;
             push(@success_files,$file);
             $fullstring =~ s/$delete_something//g;
             print $file_out_handle $fullstring;
     }
     else{
-            print "there has no same content in: ".$file."\n";
+            # print "there has no same content in: ".$file."\n";
             $fail_cnt++;
             push(@fail_files,$file);
             while(<$file_handle>){
-                print $file_out_handle "it prints something\n";
+                #print $file_out_handle "it prints something\n";
                 print $file_out_handle $_;
             }
     }
-    
-    
-    print $file_out_handle "It can add the line in the bottom\n";
 
     close $file_out_handle;
 }
+
+
     #show log
+    if ($verbose){
     print "list of succesful files: \n";
     print "@success_files","\n";
-    print "success $success_cnt files\n";
     print "list of unprocess files: \n";
     print "@fail_files","\n";
-    print "fail $fail_cnt files\n";
+    }
+    print "process $success_cnt files\n";
+    print "unrocess $fail_cnt files\n";
 
 
     close $dir_handle;
